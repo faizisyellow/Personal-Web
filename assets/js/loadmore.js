9900e8b3e4baf829962dad84702ebe878b5c70d3
faizisyellow <muhamatfaissalm@gmail.com>
2023-10-27 00:31:06 +0700

    remove scroll-reveal in loadmore future

--- assets/js/loadmore.js
+++ assets/js/loadmore.js
@@ -6,24 +6,21 @@ const Cards = {
       <div class="portfolio__link">
         <a href="https://faizisyellow.github.io/boyoftheyear/" class="portfolio__link-name" target="_blank">Visit Website<i class="bx bx-chevron-right"></i></a>
       </div>
-    </div>`
+    </div>`,
   },
 };
 
 ///PORFOLIO CARDS
 const cardContainerPortfolio = document.querySelector(".portfolio__container");
-const scrollPorfolio = ".portfolio__img";
 const loadMoreButton = document.getElementById("loadMore");
 
 function createCard(cardContainer, cardTemplate, scrollElement) {
   const cardHTML = cardTemplate;
-
   cardContainer.insertAdjacentHTML("beforeend", cardHTML);
-  sr.reveal(scrollElement, { interval: 100 });
 }
 
 loadMoreButton.addEventListener("click", () => {
   // ADD CARD MAX  3
-  createCard(cardContainerPortfolio, Cards.porfolioCards.card1, scrollPorfolio);
+  createCard(cardContainerPortfolio, Cards.porfolioCards.card1);
   loadMoreButton.style.display = "none";
 });
