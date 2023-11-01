1b800b3a00c7868ba3c607e9c6d86c8d4c56e334
faizisyellow <muhamatfaissalm@gmail.com>
2023-11-01 17:56:12 +0700

    add cards

--- assets/js/loadmore.js
+++ assets/js/loadmore.js
@@ -7,9 +7,17 @@ const Cards = {
         <a href="https://faizisyellow.github.io/boyoftheyear/" class="portfolio__link-name" target="_blank">Visit Website<i class="bx bx-chevron-right"></i></a>
       </div>
     </div>`,
+    card2: `
+    <div class="portfolio__img">
+      <img src="assets/img/flipping-card-demo.jpg" alt="" />
+    
+      <div class="portfolio__link">
+        <a href="https://faizisyellow.github.io/Flipping-Card/" class="portfolio__link-name" target="_blank">Visit Website<i class="bx bx-chevron-right"></i></a>
+      </div>
+    </div>
+    `,
   },
 };
-
 
 ///PORFOLIO CARDS
 const cardContainerPortfolio = document.querySelector(".portfolio__container");
@@ -18,11 +26,11 @@ const loadMoreButton = document.getElementById("loadMore");
 function createCard(cardContainer, cardTemplate, scrollElement) {
   const cardHTML = cardTemplate;
   cardContainer.insertAdjacentHTML("beforeend", cardHTML);
-//   src.reveal(".portfolio__img", { interval: 200 });
 }
 
 loadMoreButton.addEventListener("click", () => {
   // ADD CARD MAX  3
   createCard(cardContainerPortfolio, Cards.porfolioCards.card1);
+  createCard(cardContainerPortfolio, Cards.porfolioCards.card2);
   loadMoreButton.style.display = "none";
 });
